49bcc15810008472cff5dc385b7ea049a6f12023
Anuj <anujkrgupta21@gmail.com>
2024-10-25 19:56:03 +0530

    fetch deails of only logged in user

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -2,10 +2,16 @@ import User from "../models/userModel.js";
 
 export const dashboardController = async (req, res) => {
   try {
-    const users = await User.find().select("-password");
-    res.status(200).json(users); 
+    //>>====== fetch details of the logged-in user ========>>
+    const user = await User.findById(req.user._id).select("-password");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user);
   } catch (error) {
-    console.log("Error fetching users:", error.message);
+    console.log("Error fetching user details:", error.message);
     res.status(500).json({
       message: "Internal server error",
       error: error.message,
